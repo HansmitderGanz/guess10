87f32659ba27a090c43c4ab07cedecb72b9aa3a6
HansmitderGanz <hans_hpfner@web.de>
2025-06-24 16:43:33 +0200

    Add files via upload

---
+++ quizzehn_cards.js
@@ -0,0 +1,272 @@
+const cards = [
+  {
+    "question": "Nenne die 10 meistverkauften Automarken .",
+    "answers": [
+      "1. Toyota",
+      "2. Volkswagen",
+      "3. Ford",
+      "4. Hyundai",
+      "5. Honda",
+      "6. Chevrolet",
+      "7. Nissan",
+      "8. Kia",
+      "9. Mercedes-Benz",
+      "10. BMW"
+    ]
+  },
+  {
+    "question": "Nenne die 10 beliebtesten Sportarten weltweit.",
+    "answers": [
+      "1. Fußball",
+      "2. Basketball",
+      "3. Cricket",
+      "4. Tennis",
+      "5. Volleyball",
+      "6. Tischtennis",
+      "7. Baseball",
+      "8. Rugby",
+      "9. Golf",
+      "10. Formel 1"
+    ]
+  },
+  {
+    "question": "Nenne die 10 größten Länder nach Fläche .",
+    "answers": [
+      "1. Russland",
+      "2. Kanada",
+      "3. China",
+      "4. USA",
+      "5. Brasilien",
+      "6. Australien",
+      "7. Indien",
+      "8. Argentinien",
+      "9. Kasachstan",
+      "10. Algerien"
+    ]
+  },
+  {
+    "question": "Nenne die 10 meistverkauften Softdrinks .",
+    "answers": [
+      "1. Coca-Cola",
+      "2. Pepsi",
+      "3. Sprite",
+      "4. Fanta",
+      "5. 7UP",
+      "6. Mountain Dew",
+      "7. Dr Pepper",
+      "8. Schweppes",
+      "9. Mirinda",
+      "10. Root Beer"
+    ]
+  },
+  {
+    "question": "Nenne die 10 am meisten gestreamten Serien weltweit.",
+    "answers": [
+      "1. Stranger Things",
+      "2. Wednesday",
+      "3. The Last of Us",
+      "4. Dahmer",
+      "5. The Night Agent",
+      "6. The Mandalorian",
+      "7. Bridgerton",
+      "8. The Witcher",
+      "9. Lucifer",
+      "10. Squid Game"
+    ]
+  },
+  {
+    "question": "Nenne die 10 meistverkauften Schokoladenmarken .",
+    "answers": [
+      "1. Milka",
+      "2. Ritter Sport",
+      "3. Kinder",
+      "4. Lindt",
+      "5. Ferrero Rocher",
+      "6. Snickers",
+      "7. Mars",
+      "8. Toffifee",
+      "9. M&Ms",
+      "10. Twix"
+    ]
+  },
+  {
+    "question": "Nenne die 10 meistgesuchten Begriffe auf Google .",
+    "answers": [
+      "1. YouTube",
+      "2. Facebook",
+      "3. Google",
+      "4. WhatsApp",
+      "5. Amazon",
+      "6. Instagram",
+      "7. Gmail",
+      "8. Twitter",
+      "9. TikTok",
+      "10. Wikipedia"
+    ]
+  },
+  {
+    "question": "Nenne die 10 beliebtesten Hunderassen weltweit.",
+    "answers": [
+      "1. Labrador Retriever",
+      "2. Französische Bulldogge",
+      "3. Golden Retriever",
+      "4. Deutscher Schäferhund",
+      "5. Pudel",
+      "6. Bulldogge",
+      "7. Beagle",
+      "8. Rottweiler",
+      "9. Yorkshire Terrier",
+      "10. Dackel"
+    ]
+  },
+  {
+    "question": "Nenne die 10 meistgesprochenen Sprachen .",
+    "answers": [
+      "1. Mandarin",
+      "2. Spanisch",
+      "3. Englisch",
+      "4. Hindi",
+      "5. Arabisch",
+      "6. Bengalisch",
+      "7. Portugiesisch",
+      "8. Russisch",
+      "9. Urdu",
+      "10. Indonesisch"
+    ]
+  },
+  {
+    "question": "Nenne die 10 bevölkerungsreichsten Länder .",
+    "answers": [
+      "1. Indien",
+      "2. China",
+      "3. USA",
+      "4. Indonesien",
+      "5. Pakistan",
+      "6. Nigeria",
+      "7. Brasilien",
+      "8. Bangladesch",
+      "9. Russland",
+      "10. Mexiko"
+    ]
+  },
+  {
+    "question": "Nenne die 10 schnellsten Landtiere.",
+    "answers": [
+      "1. Gepard",
+      "2. Antilope",
+      "3. Löwe",
+      "4. Gabelbock",
+      "5. Springbock",
+      "6. Strauß",
+      "7. Grauwolf",
+      "8. Pferd",
+      "9. Afrikanischer Wildhund",
+      "10. Känguru"
+    ]
+  },
+  {
+    "question": "Nenne die 10 Länder mit den meisten Inseln.",
+    "answers": [
+      "1. Schweden",
+      "2. Norwegen",
+      "3. Kanada",
+      "4. Indonesien",
+      "5. Australien",
+      "6. Philippinen",
+      "7. Finnland",
+      "8. Japan",
+      "9. USA",
+      "10. Griechenland"
+    ]
+  },
+  {
+    "question": "Nenne die 10 höchsten Berge der Erde.",
+    "answers": [
+      "1. Mount Everest",
+      "2. K2",
+      "3. Kangchendzönga",
+      "4. Lhotse",
+      "5. Makalu",
+      "6. Cho Oyu",
+      "7. Dhaulagiri",
+      "8. Manaslu",
+      "9. Nanga Parbat",
+      "10. Annapurna"
+    ]
+  },
+  {
+    "question": "Nenne die Top 10 bekannten Superhelden.",
+    "answers": [
+      "1. Superman",
+      "2. Batman",
+      "3. Spider-Man",
+      "4. Wonder Woman",
+      "5. Iron Man",
+      "6. Captain America",
+      "7. Hulk",
+      "8. Thor",
+      "9. Wolverine",
+      "10. Deadpool"
+    ]
+  },
+  {
+    "question": "Nenne die Top 10 bekannten Küchengeräte.",
+    "answers": [
+      "1. Wasserkocher",
+      "2. Kaffeemaschine",
+      "3. Mikrowelle",
+      "4. Toaster",
+      "5. Mixer",
+      "6. Herd",
+      "7. Backofen",
+      "8. Kühlschrank",
+      "9. Geschirrspüler",
+      "10. Reiskocher"
+    ]
+  },
+  {
+    "question": "Nenne die Top 10 bekannten internationale Feiertage.",
+    "answers": [
+      "1. Neujahr",
+      "2. Weihnachten",
+      "3. Ostern",
+      "4. Halloween",
+      "5. Valentinstag",
+      "6. Tag der Arbeit",
+      "7. Weltfrauentag",
+      "8. Ramadan",
+      "9. Tag der Erde",
+      "10. Chinesisches Neujahr"
+    ]
+  },
+  {
+    "question": "Nenne die Top 10 bekannten Chemieelemente.",
+    "answers": [
+      "1. Sauerstoff",
+      "2. Wasserstoff",
+      "3. Kohlenstoff",
+      "4. Stickstoff",
+      "5. Helium",
+      "6. Eisen",
+      "7. Gold",
+      "8. Kupfer",
+      "9. Blei",
+      "10. Uran"
+    ]
+  },
+  {
+    "question": "Nenne die Top 10 bekannten Hunderassen.",
+    "answers": [
+      "1. Labrador Retriever",
+      "2. Deutscher Schäferhund",
+      "3. Golden Retriever",
+      "4. Französische Bulldogge",
+      "5. Chihuahua",
+      "6. Pudel",
+      "7. Bulldogge",
+      "8. Beagle",
+      "9. Rottweiler",
+      "10. Yorkshire Terrier"
+    ]
+  }
+];
